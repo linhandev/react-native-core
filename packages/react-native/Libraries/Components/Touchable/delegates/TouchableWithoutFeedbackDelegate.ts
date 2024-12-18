b0cfdb9c8b7d102d82559b9325a3c9fc88747efa
Arkadiusz Kasprzyk <arkadiusz.kasprzyk@swmansion.com>
2024-12-18 16:57:36 +0100

    add TouchableWithoutFeedbackDelegate

---
+++ packages/react-native/Libraries/Components/Touchable/delegates/TouchableWithoutFeedbackDelegate.ts
@@ -0,0 +1,6 @@
+/**
+ * @format
+ */
+import {BaseTouchableWithoutFeedbackDelegate} from './BaseTouchableWithoutFeedbackDelegate';
+
+export default class TouchableWithoutFeedbackDelegate extends BaseTouchableWithoutFeedbackDelegate {}
