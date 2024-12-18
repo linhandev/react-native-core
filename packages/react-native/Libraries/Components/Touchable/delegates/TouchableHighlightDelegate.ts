d86a218da6307382681527a3c4649bb82e74f33d
Arkadiusz Kasprzyk <arkadiusz.kasprzyk@swmansion.com>
2024-12-18 16:21:19 +0100

    add TouchableHighlightDelegate

---
+++ packages/react-native/Libraries/Components/Touchable/delegates/TouchableHighlightDelegate.ts
@@ -0,0 +1,6 @@
+/**
+ * @format
+ */
+import {BaseTouchableHighlightDelegate} from './BaseTouchableHighlightDelegate';
+
+export default class TouchableHighlightDelegate extends BaseTouchableHighlightDelegate {}
