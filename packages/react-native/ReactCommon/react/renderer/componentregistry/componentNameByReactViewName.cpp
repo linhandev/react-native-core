016cdf6d67ef68cb3eedefdab65e9552bb2d55e1
zichen834194 <zichen834194@163.com>
2025-04-22 12:21:59 +0200

    fix: TextArea cannot be rendered

--- packages/react-native/ReactCommon/react/renderer/componentregistry/componentNameByReactViewName.cpp
+++ packages/react-native/ReactCommon/react/renderer/componentregistry/componentNameByReactViewName.cpp
@@ -39,6 +39,8 @@ std::string componentNameByReactViewName(std::string viewName) {
       viewName = "ImageView";
     } else if (viewName == "RCTSinglelineTextInputView") {
       viewName = "SinglelineTextInputView";
+    } else if (viewName == "RCTMultilineTextInputView") {
+      viewName = "MultilineTextInputView";
     } else if (viewName == "RCTModalHostView") {
       viewName = "ModalHostView";
     } else if (viewName == "RCTRefreshControl") {
